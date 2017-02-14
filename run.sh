7af037fe5cebc1cfc6f2d8f4c7d68f390c35535e
Atsushi Nakajyo <jyotti@gmail.com>
2017-02-14 13:57:34 +0900

    Change installation location

--- run.sh
+++ run.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
+#source ./tests/init.sh
 
-#set -x
+TERRAFORM_SYMLINK_DIR="/usr/local/bin"
+TERRAFORM_INSTALL_DIR="/usr/local/src"
 
-error_exit() {
-    local message=$1
-
-    echo "[FATAL] $message" 1>&2
-    exit 1
+check_packages() {
+  set +e
+  local pkgs=""
+  command -v curl &> /dev/null || pkgs+=" curl"
+  command -v unzip &> /dev/null || pkgs+=" unzip"
+  set -e
+  if [ ! -z $pkgs ]; then
+    apt-get -y update && apt-get -y install $pkgs
+  fi
 }
 
-download_terraform() {
-  local install_path=$1
-  local version=$2
-  if [ ! -f "${install_path}/terraform_${version}_linux_amd64/terraform" ]; then
-    wget -qO ${install_path}/terraform_${version}_linux_amd64.zip https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip
-    unzip -qo "${install_path}/terraform_${version}_linux_amd64.zip" -d "${install_path}/terraform_${version}_linux_amd64"
-    rm -f ${install_path}/terraform_${version}_linux_amd64.zip
+main () {
+  local version=$1
+  local install_dir=$2
+  if [ ! -f "${install_dir}/terraform_${version}_linux_amd64/terraform" ]; then
+    check_packages
+    info "Fetching Terraform..."
+    curl -Lo "${install_dir}/terraform_${version}_linux_amd64.zip" \
+            "https://releases.hashicorp.com/terraform/${version}/terraform_${version}_linux_amd64.zip"
+    info "Installing Terraform..."
+    unzip -qo "${install_dir}/terraform_${version}_linux_amd64.zip" \
+              -d "${install_dir}/terraform_${version}_linux_amd64"
+    rm -f "${install_dir}/terraform_${version}_linux_amd64.zip"
   fi
-  ln -sf ${install_path}/terraform_${version}_linux_amd64/terraform ${install_path}/bin/terraform
-  return 0
+  ln -sf "${install_dir}/terraform_${version}_linux_amd64/terraform" "${TERRAFORM_SYMLINK_DIR}/terraform"
 }
 
 # check properties
-if [ ! -n "${WERCKER_INSTALL_TERRAFORM_VERSION}" ]; then
-  error_exit 'Please specify the version property'
+if [[ ! -n "${WERCKER_INSTALL_TERRAFORM_VERSION}" ]]; then
+  fail "Please specify the version property"
 fi
 
-# default
-# ${HOME}/jyotti/bin/terraform
-# ${HOME}/jyotti/terraform_x.x.x/terraform
-# cache
-# ${WERCKER_CACHE_DIR}/jyotti/bin/terraform
-# ${WERCKER_CACHE_DIR}/jyotti/terraform_x.x.x/terraform
-INSTALL_PATH=""
-if [ -n ${WERCKER_INSTALL_TERRAFORM_USE_CACHE} -a ${WERCKER_INSTALL_TERRAFORM_USE_CACHE} == "true" ]; then
-  INSTALL_PATH=${WERCKER_CACHE_DIR}/github/jyotti
-else
-  INSTALL_PATH=${HOME}/github/jyotti
+if [[ "${WERCKER_INSTALL_TERRAFORM_USE_CACHE}" == "true" ]]; then
+  TERRAFORM_INSTALL_DIR="${WERCKER_CACHE_DIR}${TERRAFORM_INSTALL_DIR}"
 fi
-mkdir -p ${INSTALL_PATH}/bin
-PATH=${PATH}:${INSTALL_PATH}/bin
-download_terraform ${INSTALL_PATH} ${WERCKER_INSTALL_TERRAFORM_VERSION}
+mkdir -p "${TERRAFORM_SYMLINK_DIR}"
+mkdir -p "${TERRAFORM_INSTALL_DIR}"
+
+main "${WERCKER_INSTALL_TERRAFORM_VERSION}" "${TERRAFORM_INSTALL_DIR}"
